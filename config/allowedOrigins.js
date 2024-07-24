7a050daf1819507cca6a22f6744dd2f6a79290a6
Harsh <itsharsh222@gmail.com>
2024-07-24 12:16:04 +0530

    b

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,8 +2,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://loquacious-seahorse-614b70.netlify.app'
-
+    'https://loquacious-seahorse-614b70.netlify.app',
+    'https://66a0a249ad6e39c0e2041281--stalwart-flan-92541a.netlify.app'
 ];
 
 module.exports = allowedOrigins;
